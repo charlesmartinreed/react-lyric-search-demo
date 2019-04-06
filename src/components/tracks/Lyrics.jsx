257d041bb4e62c67a811564b265787bb5ccb244e
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-06 17:33:10 -0500

    Add additional album/track details. Implement moment/react-moment to format release date.

--- src/components/tracks/Lyrics.jsx
+++ src/components/tracks/Lyrics.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
 
 class Lyrics extends Component {
   state = {
     track: {},
-    lyrics: {}
+    lyrics: {},
+    album: {}
   };
 
   componentDidMount() {
@@ -15,17 +18,83 @@ class Lyrics extends Component {
         }&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
-        console.log(res.data);
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        // console.log(res.data);
+        this.setState({ track: res.data.message.body.track });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/album.get?album_id=${
+            this.state.track.album_id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({
+          album: res.data.message.body.album
+        });
+        console.log(this.state.album);
       })
       .catch(err => console.log(err));
   }
 
+  // by this point, the state should be updated
+  // check to make sure that the track and lyric objects have been added
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+    const { track, lyrics, album } = this.state;
+    console.log(track);
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>:
+              {track.primary_genres.music_genre_list.length !== 0
+                ? track.primary_genres.music_genre_list[0].music_genre
+                    .music_genre_name
+                : " Recent Hits"}
+            </li>
+            <li className="list-group-item">
+              <strong>Explict Lyrics</strong>:{" "}
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>: {album.album_release_date}
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
