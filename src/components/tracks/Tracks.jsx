1e2abe370f3582daa6bde1991b77d5e48366098a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-05 23:30:58 -0500

    Implement Spinner component. Implement Track component. Make GET request to Top 10 Tracks API.

--- src/components/tracks/Tracks.jsx
+++ src/components/tracks/Tracks.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from "../tracks/Track";
 
 // bringing in the consumer allows us to grab hold of the Global Context
 // wrap your other components in your Consumer, enabling the use of props from the global context, regardless of how many levels deep your particular component may be
 
+// check if tracks is defined and if the array is empty
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
           console.log(value);
-          return <h1>Tracks</h1>;
+          const { track_list, heading } = value;
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner />;
+          } else {
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => (
+                    <Track key={item.track.track_id} track={item.track} />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          }
         }}
       </Consumer>
     );
--- src/context.js
+++ src/context.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
+const getTopTracksURL = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+  process.env.REACT_APP_MM_KEY
+}`;
+
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: "abc" } },
-      { track: { track_name: "123" } },
-      { track: { track_name: "xyz" } }
-    ],
+    track_list: [],
     heading: "Top 10 Tracks"
   };
+
+  componentDidMount() {
+    axios
+      .get(getTopTracksURL)
+      .then(res => {
+        // console.log(res.data);
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <Context.Provider value={this.state}>
